241373e671c3e30b39ed9e906162de66a49fe576
Sebastian Schoeller <sebastian.schoeller@gmail.com>
2013-03-14 11:14:46 +0000

    adding regional_group to odf editing

--- settings/odf.ini.append.php
+++ settings/odf.ini.append.php
@@ -1,5 +1,11 @@
 <?php /*
 
+[ODFExport]
+TemplateName=eziog.ott
+
+[ODFImport]
+RegisteredClassArray[]=regional_group
+
 [article]
 DefaultImportTitleAttribute=title
 DefaultImportBodyAttribute=body
@@ -9,5 +15,17 @@ Attribute[intro]=intro
 Attribute[body]=body
 Attribute[image]=image
 Attribute[caption]=caption
+
+[regional_group]
+DefaultImportTitleAttribute=title
+DefaultImportBodyAttribute=description
+Attribute[]
+Attribute[name]=name
+Attribute[short_name]=short_name
+Attribute[short_description]=short_description
+Attribute[description]=description
+Attribute[image]=image
+Attribute[caption]=caption
+Attribute[tags]=tags
 
 */ ?>
--- settings/websitetoolbar.ini.append.php
+++ settings/websitetoolbar.ini.append.php
@@ -1,5 +1,10 @@
 <?php /*
 
+[WebsiteToolbarSettings]
+HideODFContainerClasses[]=regional_group
+ODFDisplayClasses[]=regional_group
+
+
 [CustomTemplateSettings]
 CustomTemplateList[]=rss
 IncludeInView[rss]=full
