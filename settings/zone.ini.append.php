47503e6e39b4d5b1514a9d0f8620fefcd4cef51c
Sebastian Schoeller <sebastian.schoeller@gmail.com>
2013-03-14 15:35:45 +0000

    adding zone.ini to extension

---
+++ settings/zone.ini.append.php
@@ -0,0 +1,13 @@
+<?php /*
+
+[GlobalZoneLayout]
+ZoneTypeName=Global zone layout
+Zones[]
+Zones[]=main
+ZoneName[main]=Global zone
+ZoneThumbnail=globalzone_layout.gif
+Template=globalzonelayout.tpl
+AvailableForClasses[]
+AvailableForClasses[]=global_right_layout
+
+*/ ?>
