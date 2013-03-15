992129947d35c67bad447a26d638400a44ab59f3
Sebastian Schoeller <sebastian.schoeller@gmail.com>
2013-03-15 20:44:24 +0000

    adding classes to left menu in backend

---
+++ settings/contentstructuremenu.ini.append.php
@@ -0,0 +1,10 @@
+<?php /* #?ini charset="utf-8"?
+
+[TreeMenu]
+ShowClasses[]=contact_folder
+ShowClasses[]=file_folder
+ShowClasses[]=donor_folder
+ShowClasses[]=project_folder
+ShowClasses[]=regional_group
+ShowClasses[]=competence_group
+*/ ?>
